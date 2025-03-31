b9c4cde319f35788312a69fbf02c242eeec8494c
cccaternberg <acaternberg@cloudbees.com>
2025-03-31 09:29:00 +0200

    add rollout status

--- k8s-squid/deploySquid.sh
+++ k8s-squid/deploySquid.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-
-kubectl create ns squid
-kubectl apply -f deploy-squid.yaml -n squid
-
-kubectl exec -ti  deployment.apps/squid-dev-proxy  -- tail -f /var/log/squid/access.log
+NS_SQUID=squid
+kubectl create ns $NS_SQUID
+kubectl apply -f deploy-squid.yaml -n $NS_SQUID
+kubectl rollout status deployment squid-dev-proxy -n $NS_SQUID
+kubectl exec -n $NS_SQUID -ti  deployment.apps/squid-dev-proxy  -- tail -f /var/log/squid/access.log
